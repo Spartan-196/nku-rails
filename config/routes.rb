f6af05ed45ebdac38d0095829a9c854b78a6e4ac
James Atkinson <atkinsonj3@nku.edu>
2014-01-23 01:36:39 +0000

    Remove faults, setup REST resources to for :post

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 NkuRails::Application.routes.draw do
-  get "welcome/index"
+  #get "welcome/index"
+  resources :post
+    root to: 'welcome#index'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -54,4 +57,4 @@ NkuRails::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
