6611ef082bcd86c283909bef07e09c732ef0473c
James Atkinson <atkinsonj3@nku.edu>
2014-01-24 01:11:13 +0000

    Upto 5.7 is working form submits and displays

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,15 @@ class PostsController < ApplicationController
   
   def create
     # render text: params[:post].inspect #Debug dump submitted text to screen to test submition without page generation.
-    @post = Post.new(params[:post]) #Take submitted post and make new page
+    @post = Post.new(post_params) #Take submitted post and make new page
     @post.save #Save new page
     redirect_to @post #reroute submit user to the posted page
   end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
   private
   def post_params
     params.require(:post).permit(:title, :text)
