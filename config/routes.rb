1f831e470269f3081f54438f6827d5a2cd5687c3
James Atkinson <atkinsonj3@nku.edu>
2014-01-24 04:17:40 +0000

    form template

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 NkuRails::Application.routes.draw do
   #get "welcome/index"
     resources :posts #Adds REST resources
-    root to: 'welcome#index' #Rails Root location to start looking from
     GET    /posts/:id(.:format)      posts #show
     GET    /posts/:id(.:format)      posts #index
     DELETE /posts/:id(.:format)      posts#destroy
+    root to: 'welcome#index' #Rails Root location to start looking from
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
