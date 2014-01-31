faf1a93d00825b3a2b32b54631bca5832231eb2e
James Atkinson <atkinsonj3@nku.edu>
2014-01-31 22:37:50 +0000

    WIP: gravitar image load

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,14 @@
+require 'digest'
+
 class Student < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 2 }
   #validates :email, presence: true
+  
+  def image_url_with_fallback
+    if image.present?
+       
+    elsif email
+      "http://gavatar.com/"+MD5.hexdigest(:email)+"?d=mm"
+    end
+  end
 end
