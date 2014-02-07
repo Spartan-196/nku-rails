e4d90dc1ec58b7b20995e2f77a510e0b2a6c9c54
James Atkinson <atkinsonj3@nku.edu>
2014-02-07 21:46:56 +0000

    Read ability clean up I have removed my visual separations beween my 'def's as requested.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,10 @@
 class StudentsController < ApplicationController
-########
-  def new
-  @student = Student.new
- end
-########
- def create
+  
+def new
+    @student = Student.new
+end
+ 
+def create
   # render text: params[:student].inspect #Debug dump submitted text to screen to test submition without page generation.
   @student = Student.new(params[:student].permit(:name, :nickname, :email, :image)) 
  
@@ -15,43 +15,40 @@ class StudentsController < ApplicationController
     render 'new' #if good make new student from submited fields
   end
  end
-########
-  def show
-    @student = Student.find(params[:id])
-  end
-########   
-  def edit
-     @student = Student.find(params[:id])
-  end
-########
-def update
+
+def show
   @student = Student.find(params[:id])
  
-  if @student.update(params[:student].permit(:name, :nickname, :email, :image))
+end
+
+def edit
+   @student = Student.find(params[:id])
+end
+
+def update
+  @student = Student.find(params[:id])
+   if @student.update(params[:student].permit(:name, :nickname, :email, :image))
     redirect_to students_path
     flash[:success] = "Student updated"
   else
     render 'edit'
   end
 end
-#######
-  def index
-    @student = Student.all 
-  end
-########
-  def destroy
+ 
+def index
+  @student = Student.all 
+end
+ 
+def destroy
   @student = Student.find(params[:id])
   @student.destroy
- 
-  redirect_to students_path
-  end
-########
+    redirect_to students_path
+end
   #http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show] #requires login to student, user id dhh password secret
-########
-  private
+  
+private
   def student_params
     params.require(:student).permit(:name, :nickname, :email, :image)
   end
-########
 end 
 
