10844886a59ec0e01fc4480b11c9c3c3a7f18483
James Atkinson <atkinsonj3@nku.edu>
2014-02-08 05:09:48 +0000

    WIP Fix password creation attempting to move   # Schema: User(name:string, password_digest:string)   attr_accessible :email, :password, :password_confirm   validates :email, uniqueness:true   has_secure_password   validates_presence_of :password, :on => :create from user.rb to students.rb to fix passwords

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,11 @@ require 'digest'
 class Student < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 2 }
   #validates :email, presence: true
-  
+  # Schema: User(name:string, password_digest:string)
+  attr_accessible :email, :password, :password_confirm
+  validates :email, uniqueness:true
+  has_secure_password
+  validates_presence_of :password, :on => :create  
   def image_url_with_fallback
     if image.present?
       image
--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  # Schema: User(name:string, password_digest:string)
-  attr_accessible :email, :password, :password_confirm
-  validates :email, uniqueness:true
-  has_secure_password
-  validates_presence_of :password, :on => :create
-end
-
