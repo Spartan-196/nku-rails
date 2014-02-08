148b6500461e280a7c2d928370066beb4851a0f5
James Atkinson <atkinsonj3@nku.edu>
2014-02-08 05:28:33 +0000

    Syntax correction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 def create
   student = Student.find_by_email(params[:email])
   if student && student.authenticate(params[:password])
-    session[:student_id] = user.id #if user is authenticated set session id to user id
+    session[:student_id] = student.id #if user is authenticated set session id to user id
     redirect_to students_path(student.name), :notice => "Logged in!" #redirect to main student page with notice of successful login
   else
     flash[:notice] = "Invalid email or password" #if auth. fails, vaguely specify that they failed the login 
