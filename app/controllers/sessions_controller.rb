59901ec26e21f01ae342fe40a4e63646f94ea62d
James Atkinson <atkinsonj3@nku.edu>
2014-02-08 02:43:11 +0000

    Corrections to 'create' and redirection I previously has having some problems getting my sessions to work and followed some tutorials to try and correct things to at least feed back IDs. this commit  corrects probelms as a result mainly incorrect calls and path redirections; also added comments for self reference

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,18 @@
 class SessionsController < ApplicationController
   
 def create
-  user = User.find_by_email(params[:email])
-  if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect_to root_url, :notice => "Logged in!"
+  student = Student.find_by_email(params[:email])
+  if student && student.authenticate(params[:password])
+    session[:user_id] = user.id #if user is authenticated set session id to user id
+    redirect_to students_path, :notice => "Logged in!" #redirect to main student page with notice of successful login
   else
-    flash.now.alert = "Invalid email or password"
-    render "new"
+    flash[:notice] = "Invalid email or password" #if auth. fails, vaguely specify that they failed the login 
+    redirect to session_path
   end
 end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    session[:user_id] = nil #nullify currently set session ID 
+    redirect_to students_path, :notice => "Logged out!" #redirect with notice
   end
 end
