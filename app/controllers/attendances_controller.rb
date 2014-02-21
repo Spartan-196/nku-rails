81c76f8c8f33c12d29d1c47d841a9a2a8f467fa7
James Atkinson <atkinsonj3@nku.edu>
2014-02-21 22:59:38 +0000

    Added Date Selection
    Added assignemnt 6's date selection field for viewing attendances on
    specific days of the month

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,26 +2,37 @@ class AttendancesController < ApplicationController
   before_filter :require_login
 
   def index
+    @date = params[:date] || Date.today
     #attempt to find who is in what seat
-    now=Date.today
-    @seat_1 = Student.in_seat(1, now)
-    @seat_2 = Student.in_seat(2, now)
-    @seat_3 = Student.in_seat(3, now)
-    @seat_4 = Student.in_seat(4, now)
+    #now=Date.today
+    @seat_1 = Student.in_seat(1, @date)
+    @seat_2 = Student.in_seat(2, @date)
+    @seat_3 = Student.in_seat(3, @date)
+    @seat_4 = Student.in_seat(4, @date)
   end
   
   def new 
     @attendance = Attendance.new
   end
-    
   
   def create
-    @attendance= Attendance.new(attandances_parms) 
-    if @attendance.save
+    @attendance= Attendance.new(attandances_parms)
+    @existing_attendance=Attendance.where(:attended_on => Date.today, :student_id => !nil)
+    if (@existing_attendance== nil)
+      @attendance.save
       redirect_to attendances_path, notice: "Attendance Recorded"
     else 
-      render 'new'
+       flash[:error] = "You have already created an attendance for today."
+      render 'new' 
     end
+  end
+  
+  def self.in_seat(seat, date)
+   Student.joins(:attendances).where(attendances: {seat: seat, attended_on: date})
+  end
+
+  def self.absent(date)
+    Student.joins(:attendances).where.not(attendances: {attended_on: date})
   end
 
   private 
@@ -29,6 +40,7 @@ class AttendancesController < ApplicationController
   def attandances_parms
     params[:attendance][:student_id] = current_student.id
     params[:attendance][:attended_on] = Date.today
+    #params[:attendance][:date] = date
     params.require(:attendance).permit(:student_id, :attended_on, :seat)
   end
 end
