8119dc2c53886bc3f0ef60b0ddbc6acba15c7265
James Atkinson <atkinsonj3@nku.edu>
2014-02-28 21:53:42 +0000

    Add commented code in prep for securing page better via this contoller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #skip_before_action :require_login [:new,:create]
+  #before_filter :require_login
   private
+  
   def current_student
     @current_student ||= Student.find(session[:student_id]) if session[:student_id]
   end
