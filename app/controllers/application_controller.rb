9736f37d1ca91bf0011de078c426f7d8e245120c
James Atkinson <atkinsonj3@nku.edu>
2014-03-08 16:56:10 +0000

    Added requirement to be logged in, redid helper method current_student
    Routes will also need to be added for the redirect to login to function properly.
    That is to be done next

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  private
-  def current_student
-    @current_student ||= Student.find(session[:student_id]) if session[:student_id]
-  end
-
-  helper_method :current_student
+   before_action :require_login
   
-  def require_login
-    redirect_to sessions_path unless current_student
+  def get_current_student
+    # Find whos logged in
+    if( session[:student_id] == nil )
+      return nil
+    end
+    return Student.find( session[:student_id] )
   end
-end
+  
+  helper_method :get_current_student
+ 
+  private
+  def require_login
+    if get_current_student == nil
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_page_path # halts request cycle
+    end
+   end
+  end
