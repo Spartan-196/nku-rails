6f7bf027e2408af37b03ee7e2379154021b570d2
James Atkinson <atkinsonj3@nku.edu>
2014-03-12 03:15:36 +0000

    Added skip before filters for allowing access when site is secured
    Also added use of get_current_student from the Application contoller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,18 @@
 class SessionsController < ApplicationController
   
+  skip_before_filter :require_login, only: [:index, :create]
+  def index
+    current_student=get_current_student
+    if current_student != nil
+      redirect_to students_path
+    end
+  end
+  
 def create
   student = Student.find_by_email(params[:email])
   if student && student.authenticate(params[:password])
     session[:student_id] = student.id #if user is authenticated set session id to user id
-    redirect_to students_path(student.name), :notice => "Logged in!" #redirect to main student page with notice of successful login
+    redirect_to attendances_path, :notice => "Logged in!" #redirect to main student page with notice of successful login
   else
     flash[:error] = "Invalid email or password" #if auth. fails, vaguely specify that they failed the login 
     redirect_to sessions_path
