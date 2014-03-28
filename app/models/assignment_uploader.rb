22ef10d1694f8a0596739ab0092a2c33155f86e9
James Atkinson <atkinsonj3@nku.edu>
2014-03-28 04:53:13 +0000

    Revert uploads_controller back to a model, one for assignments and students
    Basically my refactor idea didn't work. I have moved back to using the models
    after looking up working csv handling for importing into the database tables

---
+++ app/models/assignment_uploader.rb
@@ -0,0 +1,12 @@
+class AssignmentUploader
+  def index
+  end
+  
+require 'csv'
+
+  def initialize(file)
+	CSV.parse(file.read, :headers => true) do |row|
+     	 @student= Student.find_by_email(row["email"])
+     	 Assignment.create(student_id: @stu.id,  name: row["name"], total: row["total"], score: row["score"]) 
+  end
+end
---
+++ app/models/student_uploader.rb
@@ -0,0 +1,9 @@
+class StudentUploader
+  
+require 'csv'
+
+  def initialize(file)
+	CSV.parse(file.read, :headers => true) do |row|
+     	 Student.create(email: row["email"], name: row["name"], password: row ["password"], password_confirmation: ["password"], nickname: row["nickname"], image "", admin: false)
+  end
+end
