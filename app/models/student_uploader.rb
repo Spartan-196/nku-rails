47904ecf5fd712a34994f42323ad92195132119a
James Atkinson <atkinsonj3@nku.edu>
2014-03-29 04:28:27 +0000

    Add missing end and proper indent

--- app/models/student_uploader.rb
+++ app/models/student_uploader.rb
@@ -3,7 +3,8 @@ class StudentUploader
 require 'csv'
 
   def initialize(file)
-	CSV.parse(file.read, :headers => true) do |row|
+	  CSV.parse(file.read, :headers => true) do |row|
      	 Student.create(email: row["email"], name: row["name"], password: row ["password"], password_confirmation: ["password"], nickname: row["nickname"], image "", admin: false)
+    end
   end
 end
