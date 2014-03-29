15ee8774bcfbc29448d051bd3d83f21cc9aa266b
James Atkinson <atkinsonj3@nku.edu>
2014-03-29 05:04:20 +0000

    Added if statement to CSV handling and code indenting
    Added an if statment to only create assignments if an email matches an
    exisiting student. I will be trying to port this to something simmilar to
    what was shown in class where it will also not make duplicates or
    update/override them if they occur.

--- app/models/assignment_uploader.rb
+++ app/models/assignment_uploader.rb
@@ -3,8 +3,10 @@ class AssignmentUploader
 require 'csv'
 
   def initialize(file)
-	CSV.parse(file.read, :headers => true) do |row|
-     	 @student= Student.find_by_email(row["email"])
-     	 Assignment.create(student_id: @stu.id,  name: row["name"], total: row["total"], score: row["score"]) 
+	  CSV.parse(file.read, :headers => true) do |row|
+     	if @student= Student.find_by_email(row["email"])
+        Assignment.create(student_id: @student.id,  name: row["name"], total: row["total"], score: row["score"]) 
+      end
+    end
   end
 end
